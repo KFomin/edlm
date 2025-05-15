54efa212fa5a2be4d2cb49fadf640091b18ac6b3
KFomin <fominkonstantiin@gmail.com>
2025-05-16 02:18:08 +0300

    implement dialog

--- main.js
+++ main.js
@@ -3,3 +3,28 @@ import Main from './src/Main.elm'
 let app = Main.init({
     node: document.getElementById('app'),
 })
+
+app.ports.toggleGroupDialog.subscribe(id => {
+    const dialog = document.querySelector(`#${id}`)
+
+    const handleBackdropClick = (event) => {
+        let dialogRect = dialog.getBoundingClientRect();
+        if (
+            event.clientX > dialogRect.right ||
+            event.clientX < dialogRect.left ||
+            event.clientY < dialogRect.top ||
+            event.clientY > dialogRect.bottom
+        ) {
+            dialog.close();
+        }
+    };
+
+    if (dialog.open) {
+        dialog.close();
+        dialog.removeEventListener('click', handleBackdropClick);
+    } else {
+        dialog.showModal();
+        dialog.addEventListener('click', handleBackdropClick);
+    }
+
+});
--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Browser
 import File exposing (File)
@@ -6,26 +6,21 @@ import File.Select as Select
 import Html
 import Html.Attributes as Attrs
 import Html.Events as Events
+import Set exposing (Set)
 import Task
 
 
 main : Program {} Model Msg
 main =
     Browser.document
-        { init =
-            \_ ->
-                ( { report =
-                        { headers = []
-                        , rows = []
-                        , filtered = []
-                        }
-                  }
-                , Cmd.none
-                )
+        { init = init
         , view = view
         , update = update
         , subscriptions = \_ -> Sub.none
         }
+
+
+port toggleGroupDialog : String -> Cmd msg
 
 
 type alias Model =
@@ -37,12 +32,25 @@ type alias Model =
     }
 
 
+init : {} -> ( Model, Cmd msg )
+init _ =
+    ( { report =
+            { headers = []
+            , rows = []
+            , filtered = []
+            }
+      }
+    , Cmd.none
+    )
+
+
 type Msg
     = FileRequested
     | FileSelected File
     | FileLoaded String
     | ReportFiltered String
     | HeaderClicked Int
+    | GroupDialogToggled
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -77,13 +85,6 @@ update msg ({ report } as model) =
                                                     String.contains
                                                         (String.toLower search)
                                                         (String.toLower cell)
-                                                        |> (\a ->
-                                                                let
-                                                                    _ =
-                                                                        Debug.log "a: " a
-                                                                in
-                                                                a
-                                                           )
                                                 )
                                     )
                     }
@@ -110,15 +111,18 @@ update msg ({ report } as model) =
             , Cmd.none
             )
 
+        GroupDialogToggled ->
+            ( model
+            , toggleGroupDialog "report-group-dialog"
+            )
+
 
 decodeBankReport : String -> { headers : List String, rows : List (List String), filtered : List (List String) }
 decodeBankReport string =
     let
+        report : { headers : List String, rows : List (List String) }
         report =
             toReport string
-
-        _ =
-            Debug.log "report: " report
     in
     { headers = report.headers
     , rows = report.rows
@@ -165,21 +169,51 @@ view : Model -> Browser.Document Msg
 view { report } =
     { title = "everyday life management"
     , body =
-        [ Html.div []
-            [ Html.input
-                [ Attrs.type_ "button"
-                , Attrs.value "upload file"
-                , Attrs.class "p-2 m-2 border rounded"
-                , Events.onClick FileRequested
+        [ Html.div [ Attrs.class "flex justify-between" ]
+            [ Html.div []
+                [ Html.input
+                    [ Attrs.type_ "button"
+                    , Attrs.value "upload file"
+                    , Attrs.class "p-2 m-2 border rounded"
+                    , Events.onClick FileRequested
+                    ]
+                    []
+                , Html.input
+                    [ Attrs.type_ "text"
+                    , Attrs.class "p-2 m-2 border rounded"
+                    , Attrs.placeholder "search"
+                    , Events.onInput ReportFiltered
+                    ]
+                    []
                 ]
-                []
-            , Html.input
-                [ Attrs.type_ "text"
-                , Attrs.class "p-2 m-2 border rounded"
-                , Attrs.placeholder "search"
-                , Events.onInput ReportFiltered
+            , if not (List.isEmpty report.rows) then
+                Html.input
+                    [ Attrs.type_ "button"
+                    , Attrs.value "group"
+                    , Attrs.class "p-2 m-2 border rounded"
+                    , Events.onClick GroupDialogToggled
+                    ]
+                    []
+
+              else
+                Html.span [] []
+            ]
+        , Html.node "dialog"
+            [ Attrs.id "report-group-dialog"
+            , Attrs.class "backdrop:bg-black/50 backdrop:backdrop-blur-md min-w-[500px] backdrop:fixed max-h-screen"
+            , Attrs.class "fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]"
+            ]
+            [ Html.div
+                [ Attrs.class "flex flex-col gap-2"
                 ]
-                []
+                [ Html.text "group dialog"
+                , Html.input
+                    [ Attrs.type_ "button"
+                    , Attrs.value "close"
+                    , Events.onClick GroupDialogToggled
+                    ]
+                    []
+                ]
             ]
         , Html.table
             [ Attrs.class "table-auto border-collapse border border-gray-400 max-100 w-full" ]
