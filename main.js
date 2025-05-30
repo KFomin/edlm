2794a6e1d7fa99c98b2f8725503d09a8f08e4dbc
KFomin <fominkonstantiin@gmail.com>
2025-05-30 21:25:17 +0300

    implement basic charts

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import Main from './src/Main.elm'
+import dialogPolyfill from 'dialog-polyfill'
 
 let app = Main.init({
     node: document.getElementById('app'),
 })
-
+//
 // app.ports.toggleGroupDialog.subscribe(id => {
 //     const dialog = document.querySelector(`#${id}`)
+//     dialogPolyfill.registerDialog(dialog);
 //
 //     const handleBackdropClick = (event) => {
 //         let dialogRect = dialog.getBoundingClientRect();
--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,10 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Browser
+import Chart
+import Chart.Attributes as ChartAttrs
+import Chart.Events as ChartEvents
+import Chart.Item as ChartItem
 import Dict exposing (Dict)
 import File exposing (File)
 import File.Select as Select
@@ -11,6 +15,9 @@ import Iso8601
 import Json.Decode as Decode
 import Task
 import Time
+
+
+port toggleGroupDialog : String -> Cmd msg
 
 
 main : Program {} Model Msg
@@ -30,6 +37,14 @@ type alias Model =
     , statementHeaders : Dict Int String
     , reportPage : ReportPage
     , expanded : Maybe String
+    , groupDialogData : Maybe GroupDialogData
+    , hovering : List (ChartItem.One { value : Float, index : Float, date : Time.Posix } ChartItem.Dot)
+    }
+
+
+type alias GroupDialogData =
+    { payer : String
+    , rows : List StatementRow
     }
 
 
@@ -54,6 +69,8 @@ init _ =
       , statementHeaders = Dict.empty
       , reportPage = RawReport
       , expanded = Nothing
+      , groupDialogData = Nothing
+      , hovering = []
       }
     , Cmd.none
     )
@@ -134,6 +151,9 @@ type Msg
     | CommonInfoHeaderChanged Int String
     | ReportPageButtonClicked ReportPage
     | GroupExpanded String
+    | GroupChartClicked String (List StatementRow)
+    | GroupDialogToggled
+    | OnChartPointHover (List (ChartItem.One { value : Float, index : Float, date : Time.Posix } ChartItem.Dot))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -224,6 +244,27 @@ update msg model =
                     else
                         Just string
               }
+            , Cmd.none
+            )
+
+        GroupChartClicked payer statementRows ->
+            ( { model
+                | groupDialogData =
+                    Just
+                        { payer = payer
+                        , rows = statementRows
+                        }
+              }
+            , toggleGroupDialog "report-group-dialog"
+            )
+
+        GroupDialogToggled ->
+            ( model
+            , toggleGroupDialog "report-group-dialog"
+            )
+
+        OnChartPointHover chartItems ->
+            ( { model | hovering = chartItems }
             , Cmd.none
             )
 
@@ -446,7 +487,9 @@ view ({ statement } as model) =
                 Html.div [] [ Html.text "loading" ]
 
             DefiningColumns ->
-                viewDefineColumns model.allStatementColumns model.statementHeaders model.statementHasHeaders
+                viewDefineColumns model.allStatementColumns
+                    model.statementHeaders
+                    model.statementHasHeaders
 
             Ready report ->
                 Html.div [ Attrs.class "flex flex-col justify-center items-center w-full" ]
@@ -471,14 +514,76 @@ view ({ statement } as model) =
                             viewRawReport report
 
                         GroupedByPayer ->
-                            viewGroupedByPayer { rows = Dict.values report, expanded = model.expanded }
+                            viewGroupedByPayer
+                                { rows = Dict.values report
+                                , expanded = model.expanded
+                                }
+                                model.hovering
                     ]
+        , viewGroupDialog model.groupDialogData
         ]
     }
 
 
-viewGroupedByPayer : { rows : List StatementRow, expanded : Maybe String } -> Html Msg
-viewGroupedByPayer { rows, expanded } =
+viewGroupDialog : Maybe GroupDialogData -> Html Msg
+viewGroupDialog maybeDialogData =
+    Html.node "dialog"
+        [ Attrs.id "report-group-dialog"
+        , Attrs.class "backdrop:bg-black/50 backdrop:backdrop-blur-md backdrop:fixed max-h-screen"
+        , Attrs.class "fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]"
+        , Attrs.class "min-w-[500px] w-full h-full"
+        ]
+        (case maybeDialogData of
+            Just dialogData ->
+                let
+                    chartData : List { valueOfPayment : Float, dateOfPayment : Float }
+                    chartData =
+                        dialogData.rows
+                            |> List.sortBy (.dateOfPayment >> Time.posixToMillis)
+                            |> List.map (\{ valueOfPayment, dateOfPayment } -> { valueOfPayment = valueOfPayment, dateOfPayment = Time.posixToMillis dateOfPayment |> Basics.toFloat })
+                in
+                [ Html.div
+                    [ Attrs.class "flex flex-col items-end gap-2 p-2"
+                    ]
+                    [ Html.input
+                        [ Attrs.type_ "button"
+                        , Attrs.value "close"
+                        , reportaPageButtonStyle True
+                        , Events.onClick GroupDialogToggled
+                        ]
+                        []
+                    , Chart.chart []
+                        [ Chart.xLabels []
+                        , Chart.yLabels [ ChartAttrs.withGrid ]
+                        , Chart.series .valueOfPayment
+                            [ Chart.interpolated
+                                .dateOfPayment
+                                []
+                                []
+                            ]
+                            chartData
+                        ]
+                    ]
+                ]
+
+            Nothing ->
+                []
+        )
+
+
+viewGroupedByPayer :
+    { rows : List StatementRow, expanded : Maybe String }
+    ->
+        List
+            (ChartItem.One
+                { value : Float
+                , index : Float
+                , date : Time.Posix
+                }
+                ChartItem.Dot
+            )
+    -> Html Msg
+viewGroupedByPayer { rows, expanded } hovering =
     let
         groupsByPayer : List ( { payer : String, totalSum : Float }, List StatementRow )
         groupsByPayer =
@@ -516,6 +621,37 @@ viewGroupedByPayer { rows, expanded } =
         (groupsByPayer
             |> List.map
                 (\( { payer, totalSum }, groupRows ) ->
+                    let
+                        chartData : List { value : Float, index : Float, date : String }
+                        chartData =
+                            groupRows
+                                |> List.sortBy (.dateOfPayment >> Time.posixToMillis)
+                                |> List.indexedMap
+                                    (\index { valueOfPayment, dateOfPayment } ->
+                                        { value = valueOfPayment
+                                        , index = Basics.toFloat index
+                                        , date = Iso8601.fromTime dateOfPayment
+                                        }
+                                    )
+                                |> List.foldl
+                                    (\{ value, index, date } acc ->
+                                        case Dict.get date acc of
+                                            Nothing ->
+                                                Dict.insert date { value = value, index = index } acc
+
+                                            Just dictValue ->
+                                                Dict.insert date { value = value + dictValue.value, index = dictValue.index } acc
+                                    )
+                                    Dict.empty
+                                |> Dict.toList
+                                |> List.map
+                                    (\( date, { value, index } ) ->
+                                        { value = value
+                                        , index = index
+                                        , date = date
+                                        }
+                                    )
+                    in
                     Html.div
                         [ Attrs.class "flex flex-col w-full"
                         ]
@@ -524,11 +660,45 @@ viewGroupedByPayer { rows, expanded } =
                             , Events.onClick (GroupExpanded payer)
                             ]
                             [ Html.p [] [ Html.text payer ]
-                            , Html.p [] [ Html.text (String.fromFloat totalSum) ]
+                            , Html.span [ Attrs.class "flex flex-row gap-4" ]
+                                [ Html.p [] [ Html.text (String.fromFloat totalSum) ]
+                                ]
                             ]
                          ]
                             ++ (if Just payer == expanded then
-                                    groupRows |> List.map viewRawReportRow
+                                    [ [ Chart.chart
+                                            [ ChartAttrs.height 100
+                                            , ChartAttrs.htmlAttrs [ Attrs.class "w-full px-4 pb-4 pt-24 border rounded" ]
+                                            ]
+                                            [ --, Chart.series .index
+                                              --    [ Chart.interpolated .value
+                                              --        []
+                                              --        [ ChartAttrs.circle, ChartAttrs.border "1px solid black" ]
+                                              --    ]
+                                              --    (chartData
+                                              --        |> List.map
+                                              --            (\{ value, index, date } ->
+                                              --                { index = index
+                                              --                , value = value
+                                              --                , date = date
+                                              --                }
+                                              --            )
+                                              --    )
+                                              Chart.bars
+                                                []
+                                                [ Chart.bar .value [ ChartAttrs.color "lightblue" ] ]
+                                                chartData
+                                            , Chart.barLabels
+                                                [ ChartAttrs.fontSize 4
+                                                , ChartAttrs.rotate 45
+                                                , ChartAttrs.color "stone"
+                                                , ChartAttrs.alignLeft
+                                                ]
+                                            ]
+                                      ]
+                                    , groupRows |> List.map viewRawReportRow
+                                    ]
+                                        |> List.concat
 
                                 else
                                     []
